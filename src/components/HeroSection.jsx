701824adee5e547714c51ab5260d17549c769e5f
Dev-Aniweb <anilkumarve51@gmail.com>
2026-01-27 23:46:21 +0530

    herosection

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+        <h1 className="text-4xl sm:text-6xl lg-text-7xl text-center tracking-wide">
+           Virtual Build Tools
+        <span className='bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text'>
+            {" "}
+            for Developer
+            </span>  
+        </h1>
+        <p className='mt-10 text-lg text-center text-neutral-500 max-w-4xl'>
+               Empower your creativity and bring your VR app ideas to life with our
+        intuitive development tools. Get started today and turn your imagination
+        into immersive reality!
+        </p>
+        <div className='flex justify-center my-10'>
+            <a
+            href='#'
+            className='bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-b-md'>
+                Start for free
+            </a>
+                <a
+            href='#'
+            className=' py-3 px-4 mx-3 rounded-b-md border-2'>
+              Documetation
+            </a>
+        </div>
+        <div className='flex mt-10 justify-center'>
+            <video autoPlay loop muted className='rounded w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'>
+            <source src={video1} type="video/mp4"/>
+            Your browser not support video
+            </video>
+               <video autoPlay loop muted className='rounded w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'>
+            <source src={video2} type="video/mp4"/>
+            Your browser not support video
+            </video>
+        </div>
+
+    </div>
+  )
+}
+
+export default HeroSection
