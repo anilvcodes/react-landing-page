285b7a6c5a67e0f3f1315e2e0fadd311f22d38ff
Dev-Aniweb <anilkumarve51@gmail.com>
2026-01-29 23:06:03 +0530

    change

--- src/components/WorkFlow.jsx
+++ src/components/WorkFlow.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { CheckCircle2 } from 'lucide-react';
+import codeImg from"../assets/code.jpg";
 
 const WorkFlow = () => {
   return (
-    <div>WorkFlow</div>
+    <div className='mt-20'>
+        <h2 className='text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide'>
+            Accelerate Your
+         <span className='bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text'>
+          {" "}  coding workflow
+            </span>
+
+        </h2>
+       
+    </div>
   )
 }
 
